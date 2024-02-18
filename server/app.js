2bc8719e05c9e0dad7812f5d89c1224b64a8e13a
Luxac-Developers <luxacdevelopers@gmail.com>
2024-02-19 01:54:46 +0300

    commit 1

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "aljauzy",
+  password: "rY!*ShNvc8J]Sy8",
+  database: "students",
+});
+
+app.post("/add_user", (req, res) => {
+  const sql =
+    "INSERT INTO student_details (`name`,`email`,`age`,`gender`) VALUES (?, ?, ?, ?)";
+  const values = [req.body.name, req.body.email, req.body.age, req.body.gender];
+  db.query(sql, values, (err, result) => {
+    if (err)
+      return res.json({ message: "Something unexpected has occured" + err });
+    return res.json({ success: "Student added successfully" });
+  });
+});
+
+app.get("/students", (req, res) => {
+  const sql = "SELECT * FROM student_details";
+  db.query(sql, (err, result) => {
+    if (err) res.json({ message: "Server error" });
+    return res.json(result);
+  });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port} `);
+  });
